30bdd62250741e999631170a70bee2ec3d544eae
Samuel Guzman <[chinosamg@gmail.com]>
2019-02-28 20:51:19 -0700

    upload

--- queryFortune500.sql
+++ queryFortune500.sql
@@ -1,1 +1,57 @@
+-- Table All Elements
 SELECT * FROM fortune500
+
+-- Average Profit Average_Profit
+
+SELECT AVG(Profits) AS Average_Profit FROM fortune500;
+
+-- Average Profit Average_Revenue
+
+SELECT AVG(Revenues) AS Average_Revenue FROM fortune500;
+
+-- Profits Greater Than Average
+SELECT * FROM fortune500
+WHERE Profits > (SELECT AVG(Profits) FROM fortune500);
+
+-- Profits Less Than Average
+SELECT * FROM fortune500
+WHERE Profits < (SELECT AVG(Profits) FROM fortune500);
+
+-- Revenue Greater Than Average
+SELECT * FROM fortune500
+WHERE Revenues > (SELECT AVG(Revenues) FROM fortune500);
+
+-- Revenue Less Than Average
+SELECT * FROM fortune500
+WHERE Revenues < (SELECT AVG(Revenues) FROM fortune500);
+
+-- Lowest Profit Top500
+SELECT MIN(Profits) As LowestProfit
+FROM fortune500;
+
+-- Highest Profit Top500
+SELECT MAX(Profits) As HighestProfit
+FROM fortune500;
+
+-- Lowest Profit Top500
+SELECT MIN(Revenues) As LowestRevenues
+FROM fortune500;
+
+-- Highest Profit Top500
+SELECT MAX(Revenues) As HighestRevenues
+FROM fortune500;
+
+-- Top10 Companies
+SELECT Rank, Company, Ticker, Revenues, Profits
+FROM fortune500
+GROUP BY Company
+HAVING Rank < 11
+ORDER BY Rank asc;
+
+-- Top50 Companies
+SELECT Rank, Company, Ticker, Revenues, Profits
+FROM fortune500
+GROUP BY Company
+HAVING Rank < 51
+ORDER BY Rank asc;
+
--- schemaFortune500.sql
+++ schemaFortune500.sql
@@ -2,11 +2,26 @@
 CREATE DATABASE ETL_Project_Fortune500;
 USE ETL_Project_Fortune500;
 
--- Create Two Tables
+-- Create Tables
 CREATE TABLE Fortune500 (
 Rank INT PRIMARY KEY,
     Company TEXT,
     Ticker TEXT,
     Revenues INT,
     Profits INT);
+
+CREATE TABLE Fortune100 (
+Rank INT PRIMARY KEY,
+    Company TEXT,
+    Ticker TEXT,
+    Revenues INT,
+    Profits INT);
+
+CREATE TABLE Fortune50 (
+Rank INT PRIMARY KEY,
+    Company TEXT,
+    Ticker TEXT,
+    Revenues INT,
+    Profits INT);
+
 
